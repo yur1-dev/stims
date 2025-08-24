ac2338ebfd94f808807ba70f5af6e430cc39e894
yur1-dev <yuriesb01@gmail.com>
2025-08-24 22:54:38 +0800

    updates

--- app/components/Socials/Socials.tsx
+++ app/components/Socials/Socials.tsx
@@ -25,7 +25,7 @@ export default function Social() {
     //   height: 48,
     // },
     {
-      href: "",
+      href: "https://pump.fun/coin/6PRUjbAcabg7pLuKWg1Lxa8ZBzWE4bDBuA24zZttpump",
       src: "/pumpfun.png",
       alt: "Pill Logo",
       width: 48,
